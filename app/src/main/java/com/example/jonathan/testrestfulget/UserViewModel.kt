d8507279e8b2faea71a1312a167c56043bd4632e
Sealight Clock <chbebram@hotmail.com>
2024-11-21 21:56:00 -0800

    Added "suspend" keyword to fun loadDataFromNetwrok() even though the editor suggests removing it.

--- app/src/main/java/com/example/jonathan/testrestfulget/UserViewModel.kt
+++ app/src/main/java/com/example/jonathan/testrestfulget/UserViewModel.kt
@@ -19,8 +19,20 @@ class UserViewModel : ViewModel() {
     private var _users = MutableLiveData<List<User>>(emptyList())
     var users: LiveData<List<User>> = _users
 
+    /**
+     * !!! Key keyward "suspend" even thought the editor suggests removing it.
+     * Keyword 'suspend" is not required here. However, there are many advantages of using it here:
+     * [1] This indicates that this function takes a long time to complete, so should be called
+     *     in a coroutine with "launch".
+     * [2] This can not be directly called by a non-suspend function.
+     * [3] This suspend function call call other suspend functions.
+     * [4] Because we know this function is to be called in a coroutine, we cannot use:
+     *         _users.value = newValue
+     *     Instead, we have to use:
+     *         _users.postValue(newValue)
+     */
     // This provides a list of users from the internet, with a delay:
-    fun loadDataFromNetwork() {
+    suspend fun loadDataFromNetwork() {
         Log.d(TAG, "loadDataFromNetwork")
 
         _users.postValue(repository.getUsersFromNetwork())
